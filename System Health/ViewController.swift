699e74db3f2a5905e1e46224a2006408f5324679
Adrien Humiliere <adrien.humiliere@brut.media>
2019-09-24 22:26:12 +0200

    Initial Commit

---
+++ System Health/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  System Health
+//
+//  Created by Adrien Humiliere on 24/09/2019.
+//  Copyright © 2019 adhumi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
