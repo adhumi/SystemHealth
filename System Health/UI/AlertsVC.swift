ba53e9c90081efab1d32b3bf69e7c00478bb7ab1
Adrien Humiliere <adrien.humiliere@brut.media>
2019-09-28 22:47:03 +0200

    Init RootVC and AlertsVC with Context

--- System Health/UI/AlertsVC.swift
+++ System Health/UI/AlertsVC.swift
@@ -1,9 +1,34 @@
 import UIKit
 
 class AlertsVC: UITableViewController {
+    init(context: Context) {
+        super.init(style: .plain)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         title = "System Health"
+
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: String(describing: UITableViewCell.self))
+    }
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: UITableViewCell.self), for: indexPath)
+        cell.textLabel?.text = "Soon"
+        cell.detailTextLabel?.text = "Very soon"
+        return cell
     }
 }
--- System Health/UI/RootVC.swift
+++ System Health/UI/RootVC.swift
@@ -1,7 +1,17 @@
 import UIKit
 
 class RootVC: UIViewController {
-    let alertsVC = AlertsVC(style: .plain)
+    let alertsVC: AlertsVC
+
+    init(context: Context) {
+        self.alertsVC = AlertsVC(context: context)
+        
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
